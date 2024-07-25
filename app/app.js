9e39856849a03426001b0c425e19a8f69c79649d
Amir5793 <amienakhostin48@gmail.com>
2024-07-25 08:37:18 -0700

    remake everything

--- app/app.js
+++ app/app.js
@@ -1,12 +1,23 @@
-const item = {name : 'ali'}
+const $ = document
 
-function localStorageHandler (item){
-    localStorage.setItem('usersName',JSON.stringify(item))
-}
+const logo = $.querySelector('.logo')
+const main = $.querySelector('.main')
 
-window.addEventListener('load', function(){
-    if (JSON.stringify(JSON.parse(localStorage.getItem('usersName'))) !== JSON.stringify([])) {
-        console.log(JSON.parse(localStorage.getItem('usersName')));
-    }
+main.style.display = 'none'
+logo.addEventListener('click', e => {
+    logo.style.animation = 'logo ease 2s'
+
+    logo.addEventListener('animationend', () => {
+        logo.style.display = 'none'
+        console.log('display blocked');
+        main.style.display = 'block'
+    })
+
+    console.log('hello cmd');
 })
+console.log('hello cmd');
+
+
+
 // localStorageHandler(item)
+// https://dynamite-8c5de-default-rtdb.firebaseio.com/
