d05116121f75d691bf676e6c4299d823f58e89db
Amir5793 <amienakhostin48@gmail.com>
2024-07-26 03:03:23 -0700

    restore js

--- app/app.js
+++ app/app.js
@@ -1,21 +1,21 @@
-// const $ = document
+const $ = document
 
-// const logo = $.querySelector('.logo')
-// const main = $.querySelector('.main')
+const logo = $.querySelector('.logo')
+const main = $.querySelector('.main')
 
-// main.style.display = 'none'
-// logo.addEventListener('click', e => {
-//     logo.style.animation = 'logo ease 2s'
+main.style.display = 'none'
+logo.addEventListener('click', e => {
+    logo.style.animation = 'logo ease 2s'
 
-//     logo.addEventListener('animationend', () => {
-//         logo.style.display = 'none'
-//         console.log('display blocked');
-//         main.style.display = 'block'
-//     })
+    logo.addEventListener('animationend', () => {
+        logo.style.display = 'none'
+        console.log('display blocked');
+        main.style.display = 'block'
+    })
 
-//     console.log('hello cmd');
-// })
-// console.log('hello cmd');
+    console.log('hello cmd');
+})
+console.log('hello cmd');
 
 
 
