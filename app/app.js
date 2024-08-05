0bf426caf98d1bce1c5cc4c9ba714b982665ebb3
Amir5793 <amienakhostin48@gmail.com>
2024-08-05 06:20:19 -0700

    change the logo

--- app/app.js
+++ app/app.js
@@ -1,25 +1,30 @@
 const $ = document
 
+let amethystNewSound = new Audio('amethyst.mp3')
+let amethystSound = $.querySelector('.amethyst-sound')
 const logo = $.querySelector('.logo-container')
 const logoShineSvg = $.querySelector('.logo-shine')
 const logoText = $.querySelector('.logo-text')
 const loadingContainer = $.querySelector('.loading-container')
 const loadingBarText = $.querySelector('.loading-text')
 const gameText = $.querySelector('.game-text')
+const gameLogo = $.querySelector('.game-logo')
 const progressBarContainer = $.querySelector('.progress-bar-container')
+const progressBar = $.querySelector('.progress-bar')
 const range = $.querySelector('.range')
 const main = $.querySelector('.main')
 
 main.style.display = 'none'
 
 loadingContainer.style.display  = 'none'
-logoText.style.display = 'none'
 gameText.style.display = 'none'
 progressBarContainer.style.display = 'none'
+gameLogo.style.display = 'none'
 
+amethystSound.play()
+amethystNewSound.play()
 logoShineSvg.addEventListener('animationend', (e) => {
     e.target.style.display = 'none'
-    logoText.style.display = 'inline-block'
 })
 logoText.addEventListener('animationend', e => {
     logo.style.display = 'none'
@@ -28,8 +33,13 @@ logoText.addEventListener('animationend', e => {
 loadingContainer.addEventListener('animationend', () => {
     gameText.style.display = 'inline-block'
     progressBarContainer.style.display = 'inline-block'
+    gameLogo.style.display = 'inline-block'
 })
-range.addEventListener('animationstart', () => {
+// progressBar.addEventListener('animationend', () => {
+//     loadingContainer.style.display = 'none'
+//     main.style.display = 'block'
+// })
+progressBar.addEventListener('animationstart', () => {
 let n = 1
 let loadingText = setInterval(() => {
     loadingBarText.innerHTML = n + '%'
@@ -37,13 +47,11 @@ let loadingText = setInterval(() => {
     if (n < 100){
         n++
     }else{
+        loadingContainer.style.display = 'none'
+        main.style.display = 'block'
         clearInterval(loadingText)
     }
 },60)
-})
-range.addEventListener('animationend', () => {
-    loadingContainer.style.display = 'none'
-    main.style.display = 'block'
 })
 
 
